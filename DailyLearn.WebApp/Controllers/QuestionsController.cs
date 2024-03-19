93bd2ff44ecc7290c33d187d83473f7611246416
morph <emmanuel.stephant@laposte.net>
2024-03-19 09:34:21 +0100

    Corrections sur l'ajout des réponses

--- DailyLearn.WebApp/Controllers/QuestionsController.cs
+++ DailyLearn.WebApp/Controllers/QuestionsController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using DailyLearn.DbLib.Models;
 using DailyLearn.WebApp.Data;
-using System.Numerics;
 
 namespace DailyLearn.WebApp.Controllers
 {
